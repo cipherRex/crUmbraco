b3d95c1c13d4d606e586fddab4602814e5c92e3e
Matt West <cipherRex@gmail.com>
2020-12-24 11:23:02 -0500

    .

---
+++ crUmbraco/App_Start/MemberRegistrationEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using Umbraco.Core;
+using Umbraco.Core.Events;
+using Umbraco.Core.Models;
+using Umbraco.Core.Services;
+using Umbraco.Core.Services.Implement;
+
+namespace crUmbraco.EventHandlers
+{
+    public class MemberRegistrationEventHandler : ApplicationEventHandler
+    {
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            MemberService.Created += MemberService_Created;
+        }
+
+        private void MemberService_Created(IMemberService sender, NewEventArgs<IMember> e)
+        {
+            // Always add user to "Main Client" group
+            sender.AssignRole(e.Entity.Username, "Main Client");
+            sender.Save(e.Entity);
+        }
+    }
+}
